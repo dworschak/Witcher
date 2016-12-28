219c2f338b6e6c5b9e2a23b22d45607394a33016
rquinio <romain.quinio@gmail.com>
2016-12-28 21:29:58 +0100

    Minor text fixes :)

--- MOD/Witcher/common/defines/00_override.lua
+++ MOD/Witcher/common/defines/00_override.lua
@@ -81,3 +81,5 @@ NDefines.NRulerDesigner.COST_DEFENCE = 100.0
 NDefines.NRulerDesigner.MAX_AGE = 100
 -- Add two layers (p14 and p15) for pre-rendered portraits
 NDefines.NGraphics.NUMBER_OF_PROPERTIES = 16
+-- Pruning - should exclude sorcerers and witchers
+NDefines.NEngine.HEALTH_IMMUNITY_TO_PRUNING = 7
